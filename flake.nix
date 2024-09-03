0bef05b6339385815160649857898e4372befcfd
Walther Chen <walther.chen@gmail.com>
2024-09-03 13:40:03 -0400

    update flake, odin

--- flake.nix
+++ flake.nix
@@ -2,67 +2,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
   };
 
-
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system: let
-      # TODO: only needed when nixpkgs is not up to date.
-      odin-overlay = self: super: {
-        odin = super.odin.overrideAttrs (old: rec {
-          version = "nightly-2023-08-10";
-          src = super.fetchFromGitHub {
-            owner = "odin-lang";
-            repo = "Odin";
-            rev = "589820639c38979f4c801e8edcbb62c21ca15099";
-            sha256 = "SHy3KP9xyM1taHLRwh5NnmpVUoCpalic9PKjxm2dBUo=";
-          };
-
-          nativeBuildInputs = with super; [ makeWrapper which ];
-
-          LLVM_CONFIG = "${super.llvmPackages.llvm.dev}/bin/llvm-config";
-          postPatch = ''
-            sed -i 's/^GIT_SHA=.*$/GIT_SHA=/' build_odin.sh
-            sed -i 's/LLVM-C/LLVM/' build_odin.sh
-            patchShebangs build_odin.sh
-          '';
-
-          installPhase = old.installPhase + "cp -r vendor $out/bin/vendor";
-        });
-      };
-
-      ols-overlay = self: super: {
-        ols = super.ols.overrideAttrs (old: rec {
-          version = "nightly-2023-08-09";
-          src = super.fetchFromGitHub {
-            owner = "DanielGavin";
-            repo = "ols";
-            rev = "a67fe36cf772653f75f865c997818fcb3915f2f4";
-            sha256 = "wav9unHiwFUaOMBXmgFeYzbsPoAPoP2FmNXLxTholyk=";
-          };
-
-          installPhase = old.installPhase;
-        });
-      };
-
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          (odin-overlay) (ols-overlay)
-        ];
-      };
+        overlays = [];
+        };
 
-      lib = pkgs.lib;
-      in {
+        lib = pkgs.lib;
+        in {
         devShells.default = pkgs.mkShell {
         nativeBuildInputs = [
         pkgs.odin
         pkgs.ols
+        pkgs.poop
+        pkgs.hyperfine
         ];
 
-        # for ols
-        ODIN_ROOT = "${pkgs.odin}/share";
+        # because odin error messages don't pick up light theme
+        NO_COLOR=true;
       };
     });
 }
